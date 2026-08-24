ea3acd0a2abf45c356ba0a178e2affe5ce57b703
Gabriel <gabriel.olveira120702@gmail.com>
2026-08-24 19:42:28 -0300

    fix: diagnose and connect realtime

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@ export default defineNuxtConfig({
   css: ['~/assets/main.css'],
   runtimeConfig: {
     public: {
-      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL || 'https://bpuuqblxmxnelfwalevp.supabase.co',
-      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwdXVxYmx4bXhuZWxmd2FsZXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MDYzMTksImV4cCI6MjEwMzE4MjMxOX0.Kd3_eZO2g_eOdCIXYiBlvZNP4IslhbI-LIl_QY2fAAA',
+      supabaseUrl: 'https://bpuuqblxmxnelfwalevp.supabase.co',
+      supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwdXVxYmx4bXhuZWxmd2FsZXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MDYzMTksImV4cCI6MjEwMzE4MjMxOX0.Kd3_eZO2g_eOdCIXYiBlvZNP4IslhbI-LIl_QY2fAAA',
     },
   },
 });
