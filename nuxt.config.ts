faee7efe1996ecfa90e51159991ebed053ff000b
Gabriel <gabriel.olveira120702@gmail.com>
2026-08-24 19:27:42 -0300

    fix: configure Supabase fallback

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@ export default defineNuxtConfig({
   css: ['~/assets/main.css'],
   runtimeConfig: {
     public: {
-      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL || '',
-      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY || '',
+      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL || 'https://bpuuqblxmxnelfwalevp.supabase.co',
+      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwdXVxYmx4bXhuZWxmd2FsZXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MDYzMTksImV4cCI6MjEwMzE4MjMxOX0.Kd3_eZO2g_eOdCIXYiBlvZNP4IslhbI-LIl_QY2fAAA',
     },
   },
 });
